d7381e1df4d58fc5cbcf5593a859e3ddd73a2d6a
m-okeefe <megan037@gmail.com>
2018-12-03 13:22:53 -0800

    Deploy

--- mooninfo/deploy.sh
+++ mooninfo/deploy.sh
@@ -4,8 +4,4 @@ kubectl create -f ./moonfacts.yml
 kubectl create -f ./moonphases-v1.yml
 kubectl create -f ./moonsite.yml
 
-kubectl create -f ./istio-ingress.yml
-kubectl create -f ./istio-egress.yml
-
 # kubectl create -f ./moonphases-v2.yml
-# kubectl create -f ./istio-split-traffic.yml
